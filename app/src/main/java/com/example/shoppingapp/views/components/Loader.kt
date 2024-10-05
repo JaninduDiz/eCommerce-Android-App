7389b77a0ce48dde280ad825b1dc385dbcfdf6ea
JaninduDiz <janindu001@gmail.com>
2024-10-05 20:23:31 +0530

    add loader

--- app/src/main/java/com/example/shoppingapp/views/components/Loader.kt
+++ app/src/main/java/com/example/shoppingapp/views/components/Loader.kt
@@ -1,22 +1,29 @@
 package com.example.shoppingapp.views.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CircularIndicator() {
-    CircularProgressIndicator(
-        modifier = Modifier.width(64.dp),
-        color = MaterialTheme.colorScheme.secondary,
-        trackColor = MaterialTheme.colorScheme.surfaceVariant,
-        strokeWidth = 4.dp,
-
-    )
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(64.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            strokeWidth = 4.dp,
+        )
+    }
 }
 
 @Preview(showBackground = true)
